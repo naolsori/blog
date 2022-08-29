b04fe1fe38c0941e3366897684b68a08f1c20e1d
Naol Sori <105045117+naolsori@users.noreply.github.com>
2022-08-29 10:21:31 -0700

    init

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -7,11 +7,18 @@
 <br /><br/>
 <button v-on:click=getData()>login</button>
 <br /> <br />
+<h3>Profesion</h3>
 <label for="java"> java </label>
  <label for="php"> php </label>
 <input type="checkbox" value="java" v-model="technology" id="java" />
-<input type="checkbox" value="php" v-model="technology" id="php" />
+<input type="checkbox" value="php"  v-model="technology" id="php" />
+<br /><br />
+<label for="student">student</label>
+ <input type="radio" value="student" id="student" v-model="profesion" name="profesion" />
+ <label for="developer">developer</label>
+ <input type="radio" value="developer" id="developer" v-model="profesion" name="profesion" />
 <h2>Technolgy : {{technology}}</h2>
+<h4>i am {{profesion}}</h4>
 
 </template>
 <script>
@@ -20,7 +27,8 @@ export default {
  data(){
     return{
         Data : 200,
-        technology:[]
+        technology:[],
+        profesion: null
     }
  },
  methods:{
